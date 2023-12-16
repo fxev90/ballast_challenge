851cabe1e52ce8ea17a7d143c6dd4ce8aaa16345
Francisco Escalante <fxev90@gmail.com>
2023-12-16 13:20:30 -0400

    Update user migrations and model
    - Added user type column with enum Librarian, Member
    - Add new colums to user model

--- library_api/database/migrations/2014_10_12_000000_create_users_table.php
+++ library_api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('names')->nullable();
             $table->string('last_names')->nullable();
+            $table->enum('user_type', ['Librarian', 'Member'])->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
